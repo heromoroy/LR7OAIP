d87d03e10b1898ad2aa45ff611a349ee2240ebd0
Veronika Kashina <47078407+heromoroy@users.noreply.github.com>
2020-12-18 16:21:08 +0300

    LR7OAIP

---
+++ LR7OAIP/MainApp.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Dynamic;
+using System.IO;
+
+namespace LR7OAIP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Вариант 31
+
+#if !DEBUG
+            TextWriter save_out = Console.Out;
+            TextReader save_in = Console.In;
+            var new_out = new StreamWriter(@"romb_output.txt");
+            var new_in = new StreamReader(@"romb_input.txt");
+            Console.SetOut(new_out);
+            Console.SetIn(new_in);
+#endif
+            Romb A2, B2;
+#if !DEBUG
+            A2 = Romb.GetRombFromFile();
+            A2.Info();
+            B2 = new Romb(5, 6);
+            B2.Info();
+#endif
+
+#if DEBUG
+            load.Info(ConsoleColor.Black, ConsoleColor.Red);
+#endif
+            
+#if !DEBUG
+            Console.SetOut(save_out); new_out.Close();
+            Console.SetIn(save_in); new_in.Close();
+#endif
+
+#if DEBUG
+            Console.ReadKey();
+#endif
+
+        }
+    }
+}
---
+++ LR7OAIP/Math.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Globalization;
+using System.Security.Cryptography.X509Certificates;
+
+namespace LR7OAIP
+{
+    class Romb
+    {
+
+        public double A1, B1; // первое число - горизонтальная диагональ, второе - вертикальная диагональ
+
+        public Romb() { }
+
+        public Romb(double a, double b)
+        {
+            A1 = a;
+            B1 = b;
+        }
+
+        
+
+        public void Load()
+        {
+            A1 = Convert.ToInt32(Console.ReadLine());
+            B1 = Convert.ToInt32(Console.ReadLine());
+        }
+ 
+        public static Romb GetRombFromFile()
+        {
+            double AA1 = Convert.ToDouble(Console.ReadLine());
+            double BB1 = Convert.ToDouble(Console.ReadLine());
+            return new Romb(AA1, BB1);
+        }
+
+        public void RombPic()
+        {
+            
+        }
+
+        public double SofRomb()
+        {
+            return (A1 * B1 * 0.5);
+        }
+        public double RofRomb()
+        {
+            return (Math.Sqrt((A1 / 2) * (A1 / 2) + (B1 / 2) * (B1 / 2)));
+        }
+
+        public void Info()
+        {
+#if DEBUG
+            Console.WriteLine("введите данные A и B:");
+#endif
+
+#if DEBUG
+            Console.WriteLine();
+#endif
+            String str =
+                "******* *******\n" +
+                "*****     *****\n" +
+                "***         ***\n" +
+                "*     Ромб    *\n" +
+                "***         ***\n" +
+                "*****     *****\n" +
+                "******* *******\n";
+            Console.WriteLine(str);
+            Console.WriteLine(string.Format(
+                "Горизональная диагональ = {0:0.00}, вертикальная диагональ = {0:0.00}", A1, B1));
+            Console.WriteLine("Периметр = {0:0.00} ", RofRomb());
+            Console.WriteLine("Площадь = {0:0.00} ", SofRomb());
+            Console.WriteLine();
+        }
+
+#if DEBUG
+        public void Info(ConsoleColor fg, ConsoleColor bgc)
+        {
+            Console.ForegroundColor = fg;
+            Console.BackgroundColor = bgc;
+            Console.Clear();
+            Info();
+
+        }
+#endif
+    }
+}
